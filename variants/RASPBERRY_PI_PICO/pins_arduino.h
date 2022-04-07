ba21abf969711da32d367290b4301493751e9a7c
TJ Horner <me@tjhorner.com>
2022-04-07 16:14:26 -0400

    ok

--- variants/RASPBERRY_PI_PICO/pins_arduino.h
+++ variants/RASPBERRY_PI_PICO/pins_arduino.h
@@ -64,7 +64,7 @@ static const uint8_t SCK  = PIN_SPI_SCK;
 #define HAS_UNIQUE_ISERIAL_DESCRIPTOR
 #define BOARD_VENDORID		0x2e8a
 #define BOARD_PRODUCTID		0x00c0
-#define BOARD_NAME			"RaspberryPi Pico"
+#define BOARD_NAME			"PCBusiness Card"
 
 uint8_t getUniqueSerialNumber(uint8_t* name);
 void _ontouch1200bps_();
